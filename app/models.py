145b4a59646fe803da55b53d982c083482618d98
Che.El <che.eldar@gmail.com>
2020-10-18 13:03:11 +0300

    Authentication Blueprint

--- app/models.py
+++ app/models.py
@@ -17,11 +17,8 @@ class User(db.Model):
     __tablename_ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
-
-    def __repr__(self):
-        return f'<User "{self.username}">'
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     @property
     def password(self):
@@ -33,3 +30,6 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User "{self.username}">'
