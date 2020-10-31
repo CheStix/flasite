c856daf03856bc81acb21053814b0f6d70818782
Che.El <che.eldar@gmail.com>
2020-10-31 14:32:22 +0300

    added user roles

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -3,3 +3,9 @@ from flask import Blueprint
 main = Blueprint('main', __name__)
 
 from . import views, errors
+from ..models import Permission
+
+
+@main.context_processor
+def inject_permissions():
+    return dict(Permission=Permission)
--- flasite.py
+++ flasite.py
@@ -1,6 +1,7 @@
 import os
+import click
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -9,12 +10,16 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 
 @app.cli.command()
-def test():
+@click.argument('test_names', nargs=-1)
+def test(test_names):
     """ Run the unit tests. """
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    if test_names:
+        tests = unittest.TestLoader().loadTestsFromNames(test_names)
+    else:
+        tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
